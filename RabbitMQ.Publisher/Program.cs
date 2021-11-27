163c1cc7a5f0092f14e33bdf28d9c20ae8da8cd5
M-Hasbi <kaynakhasbi1@gmail.com>
2021-11-27 19:25:11 +0100

    create : publisher Program.cs

--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+
+factory.Uri = new Uri("amqps://oqunpwrq:1x7uT2QE4Orp0UJZCSR7ArLHg5XIjhqR@woodpecker.rmq.cloudamqp.com/oqunpwrq");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.QueueDeclare("hello-queue", true, false, false);
+
+string message = "hello world";
+
+var messageBody = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(string.Empty,"hello-queue",null,messageBody);
+
+Console.WriteLine("The message has been sent.");
+
+Console.ReadLine();
--- RabbitMQ.Subscriber/Program.cs
+++ RabbitMQ.Subscriber/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+
+factory.Uri = new Uri("amqps://oqunpwrq:1x7uT2QE4Orp0UJZCSR7ArLHg5XIjhqR@woodpecker.rmq.cloudamqp.com/oqunpwrq");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+//channel.QueueDeclare("hello-queue", true, false, false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+channel.BasicConsume("hello-queue",true,consumer);
+
+consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+    Console.WriteLine("The income message:" + message);
+};
+Console.ReadLine();

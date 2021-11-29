49b1638c39d4e60a9a3e2246c7b19b88cc700d3e
M-Hasbi <kaynakhasbi1@gmail.com>
2021-11-29 13:17:28 +0100

    create : Direct Method in Publisher and Consumer side

--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -2,26 +2,55 @@
 using RabbitMQ.Client;
 using System.Linq;
 using System.Text;
+using System;
 
-var factory = new ConnectionFactory();
-
-factory.Uri = new Uri("amqps://oqunpwrq:1x7uT2QE4Orp0UJZCSR7ArLHg5XIjhqR@woodpecker.rmq.cloudamqp.com/oqunpwrq");
-
-using var connection = factory.CreateConnection();
-
-var channel = connection.CreateModel();
-
-channel.ExchangeDeclare("logs-fanout", durable: true, type: ExchangeType.Fanout);
-
-Enumerable.Range(1, 50).ToList().ForEach(x =>
+namespace RabbitMQ.Publisher
 {
-    string message = $"log {x}";
+    public enum LogNames
+    {
+        Critical = 1,
+        Error = 2,
+        Warning = 3,
+        Information = 4,
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
 
-    var messageBody = Encoding.UTF8.GetBytes(message);
+            factory.Uri = new Uri("amqps://oqunpwrq:1x7uT2QE4Orp0UJZCSR7ArLHg5XIjhqR@woodpecker.rmq.cloudamqp.com/oqunpwrq");
 
-    channel.BasicPublish("logs-fanout", "", null, messageBody);
+            using var connection = factory.CreateConnection();
 
-    Console.WriteLine($"The {message} has been sent.");
-});
+            var channel = connection.CreateModel();
 
-Console.ReadLine();
+            channel.ExchangeDeclare("logs-direct", durable: true, type: ExchangeType.Direct);
+
+            Enum.GetNames(typeof(LogNames)).ToList().ForEach(x =>
+            {
+                var routeKey = $"route-{x}";
+                var queueName = $"direct-queue-{x}";
+                channel.QueueDeclare(queueName, true, false, false);
+                channel.QueueBind(queueName, "logs-direct", routeKey, null);
+            });
+
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+            {
+                LogNames log = (LogNames)new Random().Next(1, 5);
+
+                string message = $"log-type:{log}";
+
+                var messageBody = Encoding.UTF8.GetBytes(message);
+                var routeKey = $"route-{log}";
+
+                channel.BasicPublish("logs-direct", routeKey, null, messageBody);
+
+                Console.WriteLine($"The {log} has been sent.");
+            });
+
+        }
+    }
+}
+
+
--- RabbitMQ.Subscriber/Program.cs
+++ RabbitMQ.Subscriber/Program.cs
@@ -3,6 +3,7 @@ using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System.Text;
 
+
 var factory = new ConnectionFactory();
 
 factory.Uri = new Uri("amqps://oqunpwrq:1x7uT2QE4Orp0UJZCSR7ArLHg5XIjhqR@woodpecker.rmq.cloudamqp.com/oqunpwrq");
@@ -11,18 +12,12 @@ using var connection = factory.CreateConnection();
 
 var channel = connection.CreateModel();
 
-
-
-var randomQueueName = channel.QueueDeclare().QueueName;
-
-channel.QueueBind(randomQueueName, "logs-fanout", "", null);
-
-
 channel.BasicQos(0, 1, false);
 
 var consumer = new EventingBasicConsumer(channel);
+var queueName = "direct-queue-Critical";
 
-channel.BasicConsume(randomQueueName, false, consumer); //here if we set the true, it`s gonna make queue message dissappear right after it sends it to the consumer.
+channel.BasicConsume(queueName, false, consumer); //here if we set the true, it`s gonna make queue message dissappear right after it sends it to the consumer.
 
 Console.WriteLine("Logs been listening...");
 
@@ -33,7 +28,9 @@ consumer.Received += (object sender, BasicDeliverEventArgs e) =>
     // Thread.Sleep(1500);
 
     Console.WriteLine("The income message:" + message);
+  //  File.AppendAllText("log-critical.txt", message + "\n");
 
     channel.BasicAck(e.DeliveryTag, false);
 };
 Console.ReadLine();
+
